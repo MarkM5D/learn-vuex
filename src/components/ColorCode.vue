22f6ad77fbe68b564cbc8f80cbaf63c4b40e956b
Mehmet Salih Durdu <mehmetsalihd20@gmail.com>
2023-08-16 00:18:43 +0300

    vuejs-vuex
    vuejs-vuex

---
+++ src/components/ColorCode.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input placeholder="Enter color name" type="text" v-model="colorCode">
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        colorCode: {
+            get() {
+                return this.$store.state.colorCode
+            },
+            set(newValue) {
+                return this.$store.dispatch('setColorCode', newValue)
+            }
+        }
+    },
+}
+</script>
+
+<style></style>
